1a149120f801eccde02be7c1ec5e77567a823cb3
kpoilly <poillykilyan@gmail.com>
2025-01-20 16:35:14 +0100

    aled

--- frontend/playerInput.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('keydown', function(event) {
-    if (event.key === "ArrowUp") {
-        socket.send(JSON.stringify({'key': 'up', 'player': player_id}));
-    } else if (event.key === "ArrowDown") {
-        socket.send(JSON.stringify({'key': 'down', 'player': player_id}));
-    }
-});
