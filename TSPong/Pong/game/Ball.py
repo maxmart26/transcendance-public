317b6aac30d657254b6338c36e39d5be4f14355b
kpoilly <poillykilyan@gmail.com>
2025-01-22 18:09:38 +0100

    ca marche en fait

--- TSPong/Pong/game/Ball.py
+++ TSPong/Pong/game/Ball.py
@@ -10,9 +10,7 @@ class Ball:
 		self.vx = random.choice([1, -1])
 		self.vy = random.choice([1, -1])
 
-		self.size = BALL_SIZE
 		self.speed = BALL_SPEED[difficulty]
-		#self.accel = faire en sorte que l'accel soit plus forte au moment de l'impact avec raquette
 
 	def reset(self, difficulty):
 		self.x = (FIELD_WIDTH - 20) / 2
@@ -25,18 +23,18 @@ class Ball:
 		self.x += self.vx * self.speed
 		self.y += self.vy * self.speed
 
-
 	def wall_bounce(self):
-		if self.x + self.size > (FIELD_WIDTH - 20) or self.x - self.size <= 0:
+		if self.x + BALL_SIZE >= FIELD_WIDTH or self.x <= 0:
 			self.vx *= -1
-		elif self.y + self.size > (FIELD_HEIGHT - 20) or self.y - self.size <= 0:
+		elif self.y + BALL_SIZE >= FIELD_HEIGHT or self.y <= 0:
 			self.vy *= -1
 
 	def paddle_bounce(self, paddle_x, paddle_y):
-		if paddle_x > FIELD_WIDTH / 2 and self.x == paddle_x \
-			and self.y >= paddle_y and self.y <= (paddle_y + PADDLE_HEIGHT):
+		if paddle_x > FIELD_WIDTH / 2 and self.x + BALL_SIZE >= paddle_x and self.y + BALL_SIZE >= paddle_y and self.y <= (paddle_y + PADDLE_HEIGHT):
 			self.vx *= -1
-		elif paddle_x < FIELD_WIDTH / 2 and self.x == paddle_x + PADDLE_WIDTH \
-			and self.y >= paddle_y and self.y <= (paddle_y + PADDLE_HEIGHT):
+			self.speed += 0.5
+		elif paddle_x < FIELD_WIDTH / 2 and self.x <= paddle_x + PADDLE_WIDTH and self.y + BALL_SIZE >= paddle_y and self.y <= (paddle_y + PADDLE_HEIGHT):
 			self.vx *= -1
+			self.speed += 0.5
+
 	
