26e39989c0472eeb94449319c82cc8f41e3db572
root <root@DESKTOP-I8GCES8>
2025-01-27 18:33:26 +0100

    small changes in game UI

--- Full/django/app/myapp/urls.py
+++ Full/django/app/myapp/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
 	path('create-game/', views.create_game, name = 'create-game'),
 	path('game/<uuid:match_id>/', views.game, name = 'game'),
-	path('DEBUGgame', views.DEBUGgame, name = "DEBUGgame")
 ]
