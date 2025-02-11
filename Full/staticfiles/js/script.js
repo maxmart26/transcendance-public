e110e30192b3cac04fde7e6a33132fe711f64f29
lnunez-t <nuneztor@gmail.com>
2025-02-11 08:13:39 +0100

    modif affichage latest games

--- Full/staticfiles/js/script.js
+++ Full/staticfiles/js/script.js
@@ -1039,7 +1039,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         if (!response.ok) throw new Error("Erreur de chargement des matchs");
 
         const matches = await response.json();
-        populateMatchHistory(matches);
+        History(matches);
     } catch (error) {
         console.error("Erreur :", error);
     }
@@ -1078,7 +1078,7 @@ function populateMatchHistory(matches) {
             <td>${opponentName}</td>
             <td>${new Date(match.date).toLocaleDateString()}</td>
             <td><span class="${resultClass}">${match.result}</span></td>
-            <td>${match.score[1]}/${match.score[2]}</td>
+            <td>${match.score[1]}-${match.score[2]}</td>
         `;
 
         tbody.appendChild(row);
